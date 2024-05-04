9102e84dc79030f060401f5c703b3d87d686da53
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-05-04 14:45:23 +0500

    s

--- src/Recents/Recents.js
+++ src/Recents/Recents.js
@@ -5,11 +5,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SimpleBottomNavigation from '../navigation/Navigation';
 
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from 'react-router-dom';
+
 
 class Recents  extends React.Component{
 
